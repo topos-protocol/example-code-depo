999620e099a52755635022ea0a109ea62bfe2d6b
Kirk Haines <wyhaines@gmail.com>
2024-02-05 20:30:02 -0700

    Submodules was a poor idea. Refactor to do fetches from GitHub API. Rate limits for anonymous requests might be a problem down the road?

--- spec/docs.topos.technology/create-your-messaging-protocol_spec.cr
+++ spec/docs.topos.technology/create-your-messaging-protocol_spec.cr
@@ -2,7 +2,11 @@ require "../spec_helper"
 
 describe "Create Your Messaging Protocol" do
   it "TokenSent is where it is expected" do
-    ierc20messagingprotocol = ExampleFile.new("submodules/topos-smart-contracts/contracts/interfaces/IERC20Messaging.sol")
+    ierc20messagingprotocol = ExampleFile.from_github(
+      org: "topos-protocol",
+      repo: "topos-smart-contracts",
+      path: "contracts/interfaces/IERC20Messaging.sol",
+      ref: "v3.1.0")
     ierc20messagingprotocol.at(21).should match(/event TokenSent/)
     ierc20messagingprotocol.at(27).should match(/\);/)
     ierc20messagingprotocol.between(21..27).should match(/address receiver/m)
--- src/example_file.cr
+++ src/example_file.cr
@@ -1,9 +1,39 @@
+require "http"
+require "http/headers"
+require "openssl"
+require "json"
+require "base64"
+
 class ExampleFile
   @lines = [] of String
-  @path : String
 
-  def initialize(@path)
-    @lines = File.read(@path).lines
+  def self.from_path(path)
+    new(File.read(path))
+  end
+
+  def self.from_url(url)
+    context = OpenSSL::SSL::Context::Client.new
+    headers = HTTP::Headers.new
+    headers["Accept"] = "*/*"
+    response = HTTP::Client.get(url, tls: context)
+    json_response = JSON.parse(response.body)
+    content = Base64.decode_string(json_response.as_h["content"].as_s)
+    new(content)
+  end
+
+  def self.from_github(
+    org : String,
+    repo : String,
+    path : String,
+    ref : String = "main"
+  )
+    url = "https://api.github.com/repos/#{org}/#{repo}/contents/#{path}?ref=#{ref}"
+
+    from_url(url)
+  end
+
+  def initialize(data)
+    @lines = data.lines
   end
 
   def [](pos)
